d8b98e43a1feb626dc86900391521424894762d6
Max Taunton <8091929@schoolsnet.act.edu.au>
2021-08-06 19:11:16 +1000

    Added Index.php

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
- <?php
+<?php
+
+?>
+<h1 class='text-primary'>Welcome to our Shopping Website!</h1>
+<?php if (!isset($_SESSION["username"])) : ?>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+        <div class="form-group">
+            <label>Username</label>
+            <input type="text" name="username" class="form-control" required="required"/>
+        </div>
+        <div class="form-group">
+            <label>Password</label>
+            <input type="password" name="password" class="form-control" required="required"/>
+        </div>
+
+        <center>
+            <button name="login" class="btn btn-primary"><span
+                        class="glyphicon glyphicon-log-in"></span> Login
+            </button>
+        </center>
+    </form>
+<?php endif; ?>
+</body>
+</html>
